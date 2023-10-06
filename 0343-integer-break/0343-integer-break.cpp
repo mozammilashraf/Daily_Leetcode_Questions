de953ec2b1850fc00bb839646c1491c198462ea0
mozammilashraf <96988345+mozammilashraf@users.noreply.github.com>
2023-10-06 23:40:49 +0530

    Time: 2 ms (49.96%), Space: 6.5 MB (22.36%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,34 +1,3 @@
-
-*******************************DP Approach********************************************
-    
-class Solution {
-public:
-    int dp[59];
-    int solve(int n){
-
-        if(dp[n]!= -1){
-            return dp[n];
-        }
-        if(n==1) return 1;
-        int result = INT_MIN;
-        for(int i=1;i<=n-1;i++){
-           int prod = i * max(n-i, solve(n-i));
-            result = max(result, prod);
-        }
-        return dp[n]= result;
-    }
-    int integerBreak(int n) { 
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
-        
-    }
-};
-
-Time Complexity: O(n^2)
-Space Complexity: O(n) 
-
-    ***********************************Greedy Approach:***************************************
-    
 class Solution {
 public:
     int integerBreak(int n) {
@@ -44,6 +13,3 @@ public:
         return result;
     }
 };
-
-Time Complexity: O(1)
-Space Complexity: O(1)
