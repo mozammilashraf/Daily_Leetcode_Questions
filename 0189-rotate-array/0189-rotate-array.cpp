2913dcaf06fc8da4d2ce95614d19015e9a485870
mozammilashraf <96988345+mozammilashraf@users.noreply.github.com>
2023-10-20 15:49:47 +0530

    Time: 23 ms (56.25%), Space: 25.3 MB (55.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,16 +2,37 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n= nums.size();
-        if ((n == 0) || (k <= 0)) return;
-        // Make a copy of nums
-        vector<int> numsCopy(n);
-        for (int i = 0; i < n; i++){
-            numsCopy[i] = nums[i];
-        }
+        k = k%n;
+
+        // Reverse the first n - k numbers.
+        // Index i (0 <= i < n - k) becomes n - k - i.
+        reverse(nums.begin(), nums.begin() + n - k);
         
-        // Rotate the elements.
-        for (int i = 0; i < n; i++){
-            nums[(i + k)%n] = numsCopy[i];
-        }    
+        // Reverse tha last k numbers.
+        // Index n - k + i (0 <= i < k) becomes n - i.
+        reverse(nums.begin() + n - k, nums.end());
+        
+        // Reverse all the numbers.
+        // Index i (0 <= i < n - k) becomes n - (n - k - i) = i + k.
+        // Index n - k + i (0 <= i < k) becomes n - (n - i) = i.
+        reverse(nums.begin(), nums.end());
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n= nums.size();
+//         if ((n == 0) || (k <= 0)) return;
+//         // Make a copy of nums
+//         vector<int> numsCopy(n);
+//         for (int i = 0; i < n; i++){
+//             numsCopy[i] = nums[i];
+//         }
+        
+//         // Rotate the elements.
+//         for (int i = 0; i < n; i++){
+//             nums[(i + k)%n] = numsCopy[i];
+//         }    
+//     }
+// };
