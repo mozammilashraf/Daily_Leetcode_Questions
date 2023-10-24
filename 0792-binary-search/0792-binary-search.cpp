37b3c4d7a755a9e18515dc426439d13a9171072a
mozammilashraf <96988345+mozammilashraf@users.noreply.github.com>
2023-10-24 19:49:46 +0530

    Time: 31 ms (35.59%), Space: 27.8 MB (58.53%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -5,9 +5,9 @@ public:
         int s=0, e=n-1;
         while(s<=e){
             int mid = s+(e-s)/2;
-            if(nums[mid]<target)  s= mid+1;
-            if(nums[mid]>target)  e= mid-1;
-            if(nums[mid]== target) return mid;
+            if(nums[mid]<target)  s= mid+1;  
+            else if(nums[mid]== target) return mid;
+            else e= mid-1;
         }
       return -1;  
     }
