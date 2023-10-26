91ba070dc76cf81a870e9aaf506a578f94a82a54
mozammilashraf <96988345+mozammilashraf@users.noreply.github.com>
2023-10-26 17:23:09 +0530

    Time: 17 ms (78.38%), Space: 22.7 MB (18.04%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -14,10 +14,9 @@ public:
             if((mid%2==0 && nums[mid]==nums[mid+1] )|| (mid%2==1 && nums[mid]== nums[mid-1])){
                 s= mid+1;
             }
-            if((mid%2==0 && nums[mid]== nums[mid-1]) || (mid%2 ==1 &&  nums[mid]==nums[mid+1])){
+            else{
                 e= mid-1;
-            }
-            
+            }          
         }
      return nums[mid];   
     }
